1bc6d4323e6fa99121d84ed61fae419eeac29a6f
Taishi Kasuga <kasuga@livesense.co.jp>
2017-10-23 13:13:35 +0900

    WIP $PGDATA

--- docker/db/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -e "${BASEBACKUP_FILE}" ]; then
-  tar zxf ${BASEBACKUP_FILE} -C ${PGDATA}
-  chown -R postgres:postgres ${PGDATA}
-  chmod 700 ${PGDATA}
-  rm ${BASEBACKUP_FILE}
-fi
